8758b4c8dc76fa0b97f463f450ae1a94c3061e68
Aki1781 <akipatel1781@gmail.com>
2025-09-24 00:30:49 -0400

    feat: Added star background with styling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import ThemeToggle from '../components/ThemeToggle'
+import StarBackground from '../components/StarBackground'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+        {/* Theme toggle button */}
+        <ThemeToggle />
+
+
+        {/* Background Effects */}
+        <StarBackground />
+        {/* Navbar */}
+
+
+        {/* Main Section */}
+
+
+        {/* Footer */}
+    </div>
   )
 }
 
